337e67168a149a1132e54569117b40f78cc0ed19
SnowFox-95 <321mts95@gmail.com>
2022-12-28 17:44:40 +0300

    footer+yandex_maps

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
-const swiper = new Swiper('.swiper', {
-    loop: true,
-    slidesPerView:2,
-    spaceBetween:20,
-    navigation: {
-      nextEl: '.swiper-button-right',
-      prevEl: '.swiper-button-left',
-    },
-  });
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-right",
+    prevEl: ".swiper-button-left",
+  },
+});
+
+ymaps.ready(init);
+function init(){
+    // Создание карты.
+    var myMap = new ymaps.Map("map", {
+        // Координаты центра карты.
+        // Порядок по умолчанию: «широта, долгота».
+        // Чтобы не определять координаты центра карты вручную,
+        // воспользуйтесь инструментом Определение координат.
+        center: [37.160165, -8.611954],
+        // Уровень масштабирования. Допустимые значения:
+        // от 0 (весь мир) до 19.
+        zoom: 16,
+        type: 'yandex#satellite'
+    });
+}
