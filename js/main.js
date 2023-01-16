ee923c14b12b68988311a12c820325d66caaa77e
SnowFox-95 <321mts95@gmail.com>
2023-01-16 20:47:12 +0300

    feat: adaptive_site_for_device

--- js/main.js
+++ js/main.js
@@ -1,25 +1,39 @@
+const menuBtn = document.querySelector(".menu-btn");
+const menuMobile = document.querySelector(".menu-mobile");
+
+menuBtn.addEventListener("click", () => {
+  menuMobile.classList.toggle("menu--open");
+});
+
 const swiper = new Swiper(".swiper", {
   loop: true,
-  slidesPerView: 2,
-  spaceBetween: 20,
   navigation: {
     nextEl: ".swiper-button-right",
     prevEl: ".swiper-button-left",
   },
+  breakpoints:{
+    0:{
+      slidesPerView: 1,
+    },
+    768:{
+      slidesPerView: 2,
+      spaceBetween: 20,
+    }
+  }
 });
 
 ymaps.ready(init);
-function init(){
-    // Создание карты.
-    var myMap = new ymaps.Map("map", {
-        // Координаты центра карты.
-        // Порядок по умолчанию: «широта, долгота».
-        // Чтобы не определять координаты центра карты вручную,
-        // воспользуйтесь инструментом Определение координат.
-        center: [37.160165, -8.611954],
-        // Уровень масштабирования. Допустимые значения:
-        // от 0 (весь мир) до 19.
-        zoom: 16,
-        type: 'yandex#satellite'
-    });
+function init() {
+  // Создание карты.
+  var myMap = new ymaps.Map("map", {
+    // Координаты центра карты.
+    // Порядок по умолчанию: «широта, долгота».
+    // Чтобы не определять координаты центра карты вручную,
+    // воспользуйтесь инструментом Определение координат.
+    center: [37.160165, -8.611954],
+    // Уровень масштабирования. Допустимые значения:
+    // от 0 (весь мир) до 19.
+    zoom: 16,
+    type: "yandex#satellite",
+  });
 }
